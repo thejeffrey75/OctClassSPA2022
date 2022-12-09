24ab8ddb2784190ea1675fbdbaada3fb3257bf21
Jeffrey Simon <jeffreywsimon6@yahoo.com>
2022-12-09 11:20:28 -0500

    12/9

--- Components/Main.js
+++ Components/Main.js
@@ -1,8 +1,6 @@
 
 import html from "html-literal";
+import * as views from "./views";
 
-export default()=> html`  <section id="jumbotron">
-  <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
-  <a href="index.html">"Call to Action" "Button"</a>
-</section>`
-;
+export default(state)=> html`
+ ${views[state.view](state)} `;
