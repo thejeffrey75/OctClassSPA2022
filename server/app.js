ec0f3e917a3884acda385420f8bf008437fabdc4
Jeffrey Simon <jeffreywsimon6@yahoo.com>
2022-12-13 22:26:38 -0500

    Push ATT

--- server/app.js
+++ server/app.js
@@ -1,19 +1,43 @@
-// 'Import' the http module
-const http = require("http");
-// Initialize the http server
-const server = http.createServer((request, response) => {
-    // Handle the request from http://localhost:4040/status with HTTP GET method
-    if (request.url === "/status" && request.method === "GET") {
-      // Create the headers for response
-      response.writeHead(200, { "Content-Type": "application/json" });
-      // Create the response body
-      response.write(JSON.stringify({ message: "Service healthy" }));
-      // End and return the response
-      response.end();
-    }
-  })
-  // Tell the HTTP server to start listening
-  .listen(4040);
+// 'Import' the Express module instead of http
+const express = require("express");
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+const PORT = process.env.PORT || 4040; // we use || to provide a default value
+// Initialize the Express application
+const app = express();
+
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+app.use(express.json());
+app.use(logging);
+
+// Handle the request with HTTP GET method from http://localhost:4040/status
+app.get("/status", (request, response) => {
+   response.status(200).json({ message: "Service healthy" });
+});
+
+app.get("/users/:id", (request, response) => {
+  // express adds a "params" Object to requests
+  const id = request.params.id;
+  // handle GET request for post with an id of "id"
+  response.send(JSON.stringify({ user_id: id }));
+});
+
+
+app.post("/add", (request, response) => {
+  const num1 = request.body.numberOne;
+  const num2 = request.body.numberTwo;
+  const responseBody = {
+    sum: num1 + num2
+  };
+  response.json(responseBody);
+});
+
+// Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-console.log("Listening on port 4040");
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
